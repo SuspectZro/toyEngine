ae52f748df0a0c2cb89c489f3477b5950ea8bdc4
luis ruiz <98990293+SuspectZro@users.noreply.github.com>
2022-11-30 12:13:30 -0500

    addingAssets
    added assets to be used

--- toyEngine/build/.deps/toyEngine/windows/x64/release/toyEngine.lib.d
+++ toyEngine/build/.deps/toyEngine/windows/x64/release/toyEngine.lib.d
@@ -1,4 +1,11 @@
 {
+    files = {
+        [[build\.objs\toyEngine\windows\x64\release\src\Engine.cpp.obj]],
+        [[build\.objs\toyEngine\windows\x64\release\src\GraphicsManager.cpp.obj]],
+        [[build\.objs\toyEngine\windows\x64\release\src\InputManager.cpp.obj]],
+        [[build\.objs\toyEngine\windows\x64\release\src\ScriptManager.cpp.obj]],
+        [[build\.objs\toyEngine\windows\x64\release\src\SoundManager.cpp.obj]]
+    },
     values = {
         [[C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.33.31629\bin\HostX64\x64\link.exe]],
         {
@@ -7,12 +14,5 @@
             "/opt:ref",
             "/opt:icf"
         }
-    },
-    files = {
-        [[build\.objs\toyEngine\windows\x64\release\src\Engine.cpp.obj]],
-        [[build\.objs\toyEngine\windows\x64\release\src\GraphicsManager.cpp.obj]],
-        [[build\.objs\toyEngine\windows\x64\release\src\InputManager.cpp.obj]],
-        [[build\.objs\toyEngine\windows\x64\release\src\ScriptManager.cpp.obj]],
-        [[build\.objs\toyEngine\windows\x64\release\src\SoundManager.cpp.obj]]
     }
 }
